a3903b33a848ea4182f6c49612165f43e0654d9e
facekunal <facekunal@gmail.com>
2021-07-18 16:41:34 +0530

    Implement search functionality

--- app/src/main/java/com/example/flickrcl/ui/MainActivity.kt
+++ app/src/main/java/com/example/flickrcl/ui/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.flickrcl.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+
+
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
+import androidx.appcompat.widget.SearchView
+import com.example.flickrcl.R
 import com.example.flickrcl.adapter.PhotosListAdapter
 import com.example.flickrcl.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,7 +16,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +29,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.photoList.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        val searchView = menu?.findItem(R.id.app_bar_search)?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if(query != null) {
+                    binding.photoList.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+        })
+        return true
     }
 
 }
